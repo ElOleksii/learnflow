17957665893cf8a10f07694c184e544860ff4c39
Telenchak Oleksii <letel315@gmail.com>
2026-07-01 23:31:28 +0300

    feat(reviews): add POST endpoint reviews/:reviewsId to use completeReview from ReviewsService.completeReview()
    The endpoint should be provided with user via @CurrentUser() decorator (as a authenticated user), reviewsId as URL param, and quality within a body

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Param, Body } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { type JwtPayload } from 'src/common/types';
+import { CompleteReviewDto } from './dto/complete-review.dto';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
+
+  @Post(':reviewId')
+  completeReview(
+    @CurrentUser() user: JwtPayload,
+    @Param('reviewId') reviewId: string,
+    @Body() data: CompleteReviewDto,
+  ) {
+    return this.reviewsService.completeReview(user.sub, reviewId, data);
+  }
 }
--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { applyReview } from './domain/sm-2';
+import { CompleteReviewDto } from './dto/complete-review.dto';
 
 @Injectable()
 export class ReviewsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async completeReview(userId: string, reviewId: string, quality: number) {
+  async completeReview(
+    userId: string,
+    reviewId: string,
+    data: CompleteReviewDto,
+  ) {
     const review = await this.prismaService.review.findUnique({
       where: {
         id: reviewId,
@@ -26,13 +31,13 @@ export class ReviewsService {
         interval: review.interval,
         repetitions: review.repetitions,
       },
-      quality,
+      data.quality,
     );
 
     const newNextReviewDate = new Date();
     newNextReviewDate.setDate(newNextReviewDate.getDate() + newState.interval);
 
-    const updatedReview = this.prismaService.review.update({
+    const updatedReview = await this.prismaService.review.update({
       where: {
         id: review.id,
       },
